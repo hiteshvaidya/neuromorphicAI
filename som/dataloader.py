6318b32cec7864aa881f67dcd0fc43a54180cc5a
hiteshvaidya <hv8322@rit.edu>
2023-05-07 17:27:51 -0400

    added function for saving trained model

--- som/dataloader.py
+++ som/dataloader.py
@@ -6,6 +6,7 @@ version: 1.0
 author: Manali Dangarikar
 """
 import tensorflow as tf
+import pickle as pkl
 
 def loadmnist():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -13,3 +14,14 @@ def loadmnist():
     x_test = tf.cast(x_test / 255.0, tf.float32)
     
     return (x_train, y_train), (x_test, y_test)
+
+def saveModel(model, filepath):
+    """
+    dump the model to a location in pickle format
+
+    :param model: SOM network
+    :type model: Network() object
+    :param filepath: save location
+    :type filepath: str
+    """
+    pkl.dump(model, open(filepath, 'wb'))
