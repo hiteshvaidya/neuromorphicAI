68f5d60fc40c072fed1a73ed405ea8aba3605bb2
hiteshvaidya <hv8322@rit.edu>
2023-05-08 03:02:05 -0400

    changed fields of Sample() class

--- som/sample.py
+++ som/sample.py
@@ -1,149 +1,144 @@
-# """
-# sample.py
 
-# description: This file contains code for loading one input sample
-# version: 1.0
-# author: Manali Dangarikar
-# """
 
-# # import libraries
-# import numpy as np
-# import tensorflow as tf
+# import libraries
+import numpy as np
+import tensorflow as tf
 
-# class Sample:   
-#     """
-#     class for representing one input sample
-#     """
+class Sample:   
+    """
+    class for representing one input sample
+    """
 
-#     def __init__(self, label, sizeX, sizeY, values):
-#         """
-#         constructor
+    def __init__(self, label, sizeX, sizeY, image):
+        """
+        constructor
 
-#         :param label: label of the image
-#         :type label: int
-#         :param sizeX: number of rows of pixels
-#         :type sizeX: int
-#         :param sizeY: number of columns of pixels
-#         :type sizeY: int
-#         :param values: image pixel matrix
-#         :type values: float32
-#         """
-#         self.label = label
-#         self.sizeX = sizeX
-#         self.sizeY = sizeY
-#         self.values = tf.convert_to_tensor(values, dtype=tf.float32)
+        :param label: label of the image
+        :type label: int
+        :param sizeX: number of rows of pixels
+        :type sizeX: int
+        :param sizeY: number of columns of pixels
+        :type sizeY: int
+        :param image: image pixel matrix
+        :type image: float32
+        """
+        self.label = label
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.image = tf.convert_to_tensor(image, dtype=tf.float32)
 
-#     def printSample(self):
-#         """
-#         print this sample
-#         """
-#         print("label:", self.label)
-#         print("sizeX:", self.sizeX)
-#         print("sizeY:", self.sizeY)
-#         print("values:")
-#         for row in self.values.numpy():
-#             print(row)
+    def printSample(self):
+        """
+        print this sample
+        """
+        print("label:", self.label)
+        print("sizeX:", self.sizeX)
+        print("sizeY:", self.sizeY)
+        print("values:")
+        for row in self.values.numpy():
+            print(row)
 
-#     def getStats(self):
-#         """
-#         get statistics of this sample
+    def getStats(self):
+        """
+        get statistics of this sample
 
-#         :return: mean and std
-#         :rtype: tuple
-#         """
-#         mean = tf.reduce_mean(self.values)
-#         std = tf.math.reduce_std(self.values)
-#         return (mean, std)
+        :return: mean and std
+        :rtype: tuple
+        """
+        mean = tf.reduce_mean(self.values)
+        std = tf.math.reduce_std(self.values)
+        return (mean, std)
 
-#     def getSum(self):
-#         """
-#         get sum of all pixels in this sample
+    def getSum(self):
+        """
+        get sum of all pixels in this sample
 
-#         :return: sum of pixels
-#         :rtype: float32
-#         """
-#         total = tf.reduce_sum(self.values)
-#         return (total)
+        :return: sum of pixels
+        :rtype: float32
+        """
+        total = tf.reduce_sum(self.values)
+        return (total)
 
-#     def getValues(self):
-#         """
-#         Accesor for values of image
+    def getValues(self):
+        """
+        Accesor for values of image
 
-#         :return: image pixel values
-#         :rtype: float32
-#         """
-#         return (self.values)
+        :return: image pixel values
+        :rtype: float32
+        """
+        return (self.values)
 
-#     def getLabel(self):
-#         """
-#         label of the image
+    def getLabel(self):
+        """
+        label of the image
 
-#         :return: label
-#         :rtype: int
-#         """
-#         return (self.label)
+        :return: label
+        :rtype: int
+        """
+        return (self.label)
 
-#     def setLabel(self, label):
-#         """
-#         Mutator for label of the sample
+    def setLabel(self, label):
+        """
+        Mutator for label of the sample
 
-#         :param label: image label
-#         :type label: int
-#         """
-#         self.label = label
+        :param label: image label
+        :type label: int
+        """
+        self.label = label
 
-#     def write(self, file):
-#         """
-#         write this sample to a file
+    def write(self, file):
+        """
+        write this sample to a file
 
-#         :param file: filename
-#         :type file: str
-#         """
-#         file.write(str(self.label) + "\n")
-#         file.write(str(self.sizeX) + "\n")
-#         file.write(str(self.sizeY) + "\n")
-#         for row in self.values.numpy():
-#             file.write(" ".join([str(val) for val in row]) + "\n")
+        :param file: filename
+        :type file: str
+        """
+        file.write(str(self.label) + "\n")
+        file.write(str(self.sizeX) + "\n")
+        file.write(str(self.sizeY) + "\n")
+        for row in self.values.numpy():
+            file.write(" ".join([str(val) for val in row]) + "\n")
 
-#     def read(self, file):
-#         """
-#         Read a sample from a file
+    def read(self, file):
+        """
+        Read a sample from a file
 
-#         :param file: filename
-#         :type file: str
-#         """
-#         self.label = int(file.readline())
-#         self.sizeX = int(file.readline())
-#         self.sizeY = int(file.readline())
-#         self.values = []
-#         for _ in range(self.sizeX):
-#             row = [float(val) for val in file.readline().split()]
-#             self.values.append(row)
-#         self.values = tf.convert_to_tensor(self.values, dtype=tf.float32)
+        :param file: filename
+        :type file: str
+        """
+        self.label = int(file.readline())
+        self.sizeX = int(file.readline())
+        self.sizeY = int(file.readline())
+        self.values = []
+        for _ in range(self.sizeX):
+            row = [float(val) for val in file.readline().split()]
+            self.values.append(row)
+        self.values = tf.convert_to_tensor(self.values, dtype=tf.float32)
 
-#     def RMS_value(self, s):
-#         """
-#         distance of this sample from a given matrix
+    def RMS_value(self, s):
+        """
+        distance of this sample from a given matrix
 
-#         :param s: given matrix
-#         :type s: float32
-#         :return: RMS distance value
-#         :rtype: float32
-#         """
-#         output = 0.0
-#         for i in range(self.sizeX):
-#             for j in range(self.sizeY):
-#                 output += tf.math.square(self.values[i][j] - s.values[i][j])
+        :param s: given matrix
+        :type s: float32
+        :return: RMS distance value
+        :rtype: float32
+        """
+        output = 0.0
+        for i in range(self.sizeX):
+            for j in range(self.sizeY):
+                output += tf.math.square(self.values[i][j] - s.values[i][j])
 
-#         output /= self.sizeX*self.sizeY
-#         output = tf.math.sqrt(output)
-#         return output.numpy()
+        output /= self.sizeX*self.sizeY
+        output = tf.math.sqrt(output)
+        return output.numpy()
 
 """
-This file contains program to store one input sample for the SOM
+sample.py
 
-Author: Hitesh Vaidya
-Version: 1.0
+description: This file contains code for loading one input sample
+version: 1.0
+author: Manali Dangarikar
 """
 
 import numpy as np
