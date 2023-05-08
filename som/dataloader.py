d48df26c979d4a5911173c72865f00edece03893
hiteshvaidya <hv8322@rit.edu>
2023-05-08 00:52:28 -0400

    replaced field and function names

--- som/dataloader.py
+++ som/dataloader.py
@@ -7,6 +7,10 @@ author: Manali Dangarikar
 """
 import tensorflow as tf
 import pickle as pkl
+import os
+from sample import Sample
+import numpy as np
+
 
 def loadmnist():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -25,3 +29,25 @@ def saveModel(model, filepath):
     :type filepath: str
     """
     pkl.dump(model, open(filepath, 'wb'))
+
+def loadSplitData(path, class_number):
+    """
+    Load class specific data
+
+    :param path: file path
+    :type path: str
+    :param class_number: current class number
+    :type class_number: int
+    :return: samples
+    :rtype: numpy array of Sample() objects
+    """
+    samples = pkl.load(open(os.path.join(path, str(class_number) + ".pkl"), 'rb'))
+    return samples
+
+def loadClassIncremental(path, taskNumber, taskSize):
+    samples = np.array([])
+    for t in range(taskNumber*taskSize, (taskNumber+1)*taskSize):
+        task_samples = loadSplitData(path, t)
+        samples = np.concatenate([samples, task_samples])
+    np.random.shuffle(samples)
+    return samples
