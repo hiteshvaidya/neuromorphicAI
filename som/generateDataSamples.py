b10a141cdb733fdc2bf27dd8ec6ad74c3c50c0d8
hiteshvaidya <hv8322@rit.edu>
2023-05-16 04:53:15 -0700

    Save sample objects of dataset in their folder

---
+++ som/generateDataSamples.py
@@ -0,0 +1,49 @@
+import dataloader
+import numpy as np
+
+def generateTrainSamples(dataset):
+    samples = dataloader.loadNistTrainData("../data/" + dataset)
+
+    images = []
+    labels = []
+    for index, sample in enumerate(samples):
+        images.append(sample.getImage())
+        labels.append(sample.getLabel())
+        
+
+    images = np.asarray(images)
+    labels = np.asarray(labels)
+
+    print("train images: ", images.shape)
+    print("train labels: ", labels.shape)
+
+    dataloader.dumpSplitData(images, labels, 10, "../data/" + dataset + "/train")
+
+def generateTestSamples(dataset):
+    samples = dataloader.loadNistTestData("../data/" + dataset)
+
+    images = []
+    labels = []
+    for index, sample in enumerate(samples):
+        images.append(sample.getImage())
+        labels.append(sample.getLabel())
+        
+
+    images = np.asarray(images)
+    labels = np.asarray(labels)
+
+    print("test images: ", images.shape)
+    print("test labels: ", labels.shape)
+
+    dataloader.dumpSplitData(images, labels, 10, "../data/" + dataset + "/test")
+
+
+if __name__ == "__main__":
+    generateTrainSamples("mnist")
+    generateTestSamples("mnist")
+
+    generateTrainSamples("fashion")
+    generateTestSamples("fashion")
+
+    generateTrainSamples("kmnist")
+    generateTestSamples("kmnist")
