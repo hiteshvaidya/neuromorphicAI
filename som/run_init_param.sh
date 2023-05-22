ae2eb708b6f6c51d14b148cd63489cd933a91147
hiteshvaidya <hv8322@rit.edu>
2023-05-22 02:41:18 -0400

    updated run files

--- som/run_init_param.sh
+++ som/run_init_param.sh
@@ -1,6 +1,21 @@
-CUDA_VISIBLE_DEVICES=0 python network.py -u 15 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-2 -d mnist -tr 8 -tlr 45 &
-CUDA_VISIBLE_DEVICES=1 python network.py -u 20 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-3 -d mnist -tr 8 -tlr 45 &
-CUDA_VISIBLE_DEVICES=2 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-4 -d mnist -tr 12 -tlr 45 &
-CUDA_VISIBLE_DEVICES=3 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-5 -d mnist -tr 14 -tlr 40 &
-CUDA_VISIBLE_DEVICES=5 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-6 -d mnist -tr 15 -tlr 45 &
-CUDA_VISIBLE_DEVICES=6 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-7 -d mnist -tr 15 -tlr 50 &
+# CUDA_VISIBLE_DEVICES=0 python network.py -u 15 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-2 -d mnist -tr 8 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=1 python network.py -u 20 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-3 -d mnist -tr 8 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=2 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-4 -d mnist -tr 12 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=3 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-5 -d mnist -tr 14 -tlr 40 &
+# CUDA_VISIBLE_DEVICES=4 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-6 -d mnist -tr 15 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=5 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-7 -d mnist -tr 15 -tlr 50 &
+
+# CUDA_VISIBLE_DEVICES=0 python network.py -u 15 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-2 -d mnist -tr 8 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=1 python network.py -u 20 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-3 -d mnist -tr 8 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=2 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-4 -d mnist -tr 12 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=3 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-5 -d mnist -tr 14 -tlr 40 &
+# CUDA_VISIBLE_DEVICES=4 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-6 -d mnist -tr 15 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=5 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp mnist_trial-7 -d mnist -tr 15 -tlr 50 &
+
+CUDA_VISIBLE_DEVICES=0 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-2 -d fashion -tr 8 -tlr 50 &
+CUDA_VISIBLE_DEVICES=1 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-3 -d fashion -tr 5 -tlr 50 &
+CUDA_VISIBLE_DEVICES=2 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-4 -d fashion -tr 5 -tlr 45 &
+CUDA_VISIBLE_DEVICES=3 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-7 -d fashion -tr 4 -tlr 40 &
+CUDA_VISIBLE_DEVICES=4 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-8 -d fashion -tr 3 -tlr 40 &
+CUDA_VISIBLE_DEVICES=5 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-5 -d fashion -tr 2 -tlr 40 &
+CUDA_VISIBLE_DEVICES=6 python network.py -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp fashion_trial-6 -d fashion -tr 2 -tlr 35 &
--- som/run_recurrent_param.sh
+++ som/run_recurrent_param.sh
@@ -1,3 +1,10 @@
-CUDA_VISIBLE_DEVICES=4 python network.py -g 0 -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.05 -fp mnist-recurrent_decay-1 -d mnist &
-CUDA_VISIBLE_DEVICES=5 python network.py -g 1 -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.05 -fp fashion-recurrent_decay-1 -d fashion &
-CUDA_VISIBLE_DEVICES=6 python network.py -g 2 -u 25 -v 0.5 -va 0.9 -r 1.5 -lr 0.05 -fp kmnist-recurrent_decay -d kmnist &
+# CUDA_VISIBLE_DEVICES=0 python network.py -u 50 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp kmnist_trial-2 -d kmnist -tr 8 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=1 python network.py -u 50 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp kmnist_trial-3 -d kmnist -tr 8 -tlr 40 &
+# CUDA_VISIBLE_DEVICES=2 python network.py -u 50 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp kmnist_trial-4 -d kmnist -tr 6 -tlr 50 &
+# CUDA_VISIBLE_DEVICES=3 python network.py -u 50 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp kmnist_trial-5 -d kmnist -tr 6 -tlr 40 &
+# CUDA_VISIBLE_DEVICES=4 python network.py -u 50 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp kmnist_trial-6 -d kmnist -tr 4 -tlr 45 &
+# CUDA_VISIBLE_DEVICES=5 python network.py -u 50 -v 0.5 -va 0.9 -r 1.5 -lr 0.07 -fp kmnist_trial-7 -d kmnist -tr 3 -tlr 45 &
+
+CUDA_VISIBLE_DEVICES=0 python gridSearch.py -dc mnist
+CUDA_VISIBLE_DEVICES=1 python gridSearch.py -dc fashion
+CUDA_VISIBLE_DEVICES=2 python gridSearch.py -dc kmnist
